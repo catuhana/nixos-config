68f1bced86923cd2d3f5df715e64e5ca30e0ce27
tuhana <tuhana.cat+git@gmail.com>
2025-12-26 13:35:39 +0300

    Add hostname

--- networking.nix
+++ networking.nix
@@ -1,9 +1,14 @@
 { ... }:
 {
   networking = {
+    hostName = "MateBookD14";
+  
     nameservers = [ "1.1.1.1" "1.0.0.1"];
     timeServers = [ "time.cloudflare.com" ];
 
+    # Causes hangs on rebuilds,
+    # disable for now.
+    # useNetworkd = true;
     wireless.iwd.enable = true;
   };
 }
