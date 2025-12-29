d2469600f8f0f2ebb84a022c363d099cd002371e
tuhana <tuhana.cat+git@gmail.com>
2025-12-29 18:01:58 +0300

    Use Intel's Xe driver

--- hosts/MateBookD14/boot.nix
+++ hosts/MateBookD14/boot.nix
@@ -1,4 +1,11 @@
 { pkgs, ... }:
 {
-  boot.plymouth.enable = true;
+  boot = {
+    kernelParams = [
+      "i915.force_probe=!46a6"
+      "xe.force_probe=46a6"
+    ];
+
+    plymouth.enable = true;
+  };
 }
